fa76ea6502486928b1b0b3c57b6ec6c3e0733e13
forma99 <saristide99@yahoo.fr>
2025-05-18 22:42:48 -0400

     idreviws

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,22 +6,18 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-  # Liste de tous les dealers, ou filtrée par état
-    path('get_dealers/', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>/', views.get_dealerships, name='get_dealers_by_state'),
+    # # path for registration
+# route pour l’authentification
+     path(route='login', view=views.login_user, name='login'),
 
-    # Détail d’un dealer
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-
-    # Reviews d’un dealer
-    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
-
-    # Poster un review
-    path('postreview/<int:dealer_id>/', views.add_review, name='add_review'),
-
-    # Autres endpoints
-    path('get_cars/', views.get_cars, name='get_cars'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_request, name='logout'),
-    path('registration/', views.registration, name='registration'),
+    # route pour la déconnexion
+    path('logout/', view=views.logout_request, name='logout'),
+    # path for dealer reviews view
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>/', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>/', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review/', view=views.add_review, name='add_review'),
+    path(route='get_cars/', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
