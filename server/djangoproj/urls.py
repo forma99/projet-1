7206ca91a2fb3d83fbce3ba54ef168d62d97023e
forma99 <63470157+forma99@users.noreply.github.com>
2025-05-27 17:03:44 -0400

    corre url

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,22 +1,8 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView  # Import TemplateView
 from djangoapp import views  # Import custom views from djangoapp
 
 urlpatterns = [
@@ -29,6 +15,14 @@ urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
     path('logout/', TemplateView.as_view(template_name="index.html"), name='logout'),
     path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
-    path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='dealer_detail'),
-    path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='post_review'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
+    path(
+        'dealer/<int:dealer_id>/',
+        TemplateView.as_view(template_name="index.html"),
+        name='dealer_detail',
+    ),
+    path(
+        'postreview/<int:dealer_id>/',
+        TemplateView.as_view(template_name="index.html"),
+        name='post_review',
+    ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
