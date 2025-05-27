2a9eed07a525a14291a304204cb0b2db95c5ed01
forma99 <saristide99@yahoo.fr>
2025-05-27 13:52:42 -0400

    add review mk

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -15,24 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views  # Import custom views from djangoapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('logout/', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>/',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
- 
-  
+    path('about/', views.about_view, name='about'),
+    path('', views.home_view, name='home'),
+    path('contact/', views.contact_view, name='contact'),
+    path('login/', TemplateView.as_view(template_name="index.html"), name='login'),
+    path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
+    path('logout/', TemplateView.as_view(template_name="index.html"), name='logout'),
+    path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
+    path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='dealer_detail'),
+    path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='post_review'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
